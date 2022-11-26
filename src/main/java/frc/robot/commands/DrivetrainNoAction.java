e0e5e5ad909fec2fe4abc3367d4f4155b7a6253d
mrhelland <tim.helland@lebanon.k12.or.us>
2022-11-26 13:37:04 -0800

    Remove unused imports.

--- src/main/java/frc/robot/commands/DrivetrainNoAction.java
+++ src/main/java/frc/robot/commands/DrivetrainNoAction.java
@@ -5,27 +5,9 @@
 package frc.robot.commands;
 
 //import com.ctre.phoenix.sensors.PigeonIMU;
-import com.kauailabs.navx.frc.AHRS;
-import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
-import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
-import com.swervedrivespecialties.swervelib.SwerveModule;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
-import edu.wpi.first.wpilibj.SPI;
 
-import static frc.robot.Constants.SwerveDrive.*;
-
-import java.lang.module.ModuleDescriptor.Requires;
-
-import static frc.robot.Constants.*;
 
 public class DrivetrainNoAction extends CommandBase {
   /**
